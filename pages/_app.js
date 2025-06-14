c2e57319ec8166a5c7f72ef66f8f77b9d43113ce
Yusup Juniadi <yusup.juniadi@outlook.com>
2025-06-14 23:27:37 +0700

    fix: add google-site-verification to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,23 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import { store } from "../Redux/store";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="tLIZwa9r9NjkQ5jb2vrZle3-BSCCwDgQZrZ3lAZX8uQ"
+        />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </>
   );
 }
 
