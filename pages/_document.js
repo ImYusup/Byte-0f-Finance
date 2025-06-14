b7777248d945411fedfde1b879e2c038ea457259
Yusup Juniadi <yusup.juniadi@outlook.com>
2025-06-14 23:07:40 +0700

    site verify

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="tLIZwa9r9NjkQ5jb2vrZle3-BSCCwDgQZrZ3lAZX8uQ" />
+
         {/* Google AdSense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7448328675787714"
           crossOrigin="anonymous"
-        ></script>
+        />
 
-        {/* Google Analytics (gtag.js) */}
+        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZH2XKT7364"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -19,9 +22,7 @@ export default function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-ZH2XKT7364', {
-                page_path: window.location.pathname,
-              });
+              gtag('config', 'G-ZH2XKT7364');
             `,
           }}
         />
@@ -33,20 +34,17 @@ export default function Document() {
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+window.location.search;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GT-PH3HSDZ9');
             `,
           }}
         />
-
-        {/* Google Site Verification */}
-        <meta name="google-site-verification" content="tLIZwa9r9NjkQ5jb2vrZle3-BSCCwDgQZrZ3lAZX8uQ" />
       </Head>
       <body>
         <Main />
 
-        {/* Google Tag Manager (noscript fallback) */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GT-PH3HSDZ9"
